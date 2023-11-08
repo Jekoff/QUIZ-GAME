1b03e941229245c386351bdf36eb0b03a77da263
Jérôme Lanloze <99019668+Jekoff@users.noreply.github.com>
2023-11-08 19:33:56 +0100

    commit

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 const prompt = require("prompt-sync")();
 
-const value = prompt("Enter something: ");
+console.log("Welcome to the computer Hardware Quiz!")
 
-console.log(value1)
+let correct_Answers = 0;
+
+
+const answer1 = prompt("What is the brain of the computer? ");
+const correct_Answer1 = "CPU";
+
+// Use toUpperCase() to ensure that the comparison is not case-sensitive.
+if(answer1.toUpperCase() === correct_Answer1.toUpperCase()) {
+    console.log("correct!");
+    correct_Answers ++;
+} else {
+    console.log("incorrect!");
+}
+
+
+const answer2 = prompt("What is better: a 3090ti or a 4060? ");
+const correct_Answer2 = "3090TI"; // Ensure the 'correct' answer is in the same case as expected for comparison
+
+// Again, use toUpperCase() for both user input and correct answer.
+if(answer2.toUpperCase() === correct_Answer2.toUpperCase()) {
+    console.log("correct!");
+    correct_Answers ++;
+} else {
+    console.log("incorrect!");
+}
+
+
+const answer3 = prompt("What is the recommended amount of RAM in 2023? ");
+const correct_Answer3 = "16GB"; // Ensure the 'correct' answer is in the same case as expected for comparison
+
+// Again, use toUpperCase() for both user input and correct answer.
+if(answer3.toUpperCase() === correct_Answer3.toUpperCase()) {
+    console.log("correct!");
+    correct_Answers ++;
+} else {
+    console.log("incorrect!");
+}
